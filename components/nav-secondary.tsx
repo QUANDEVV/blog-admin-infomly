cb05855488000b01f7bc4aa7f6a7794ab2ac58ce
k-dev <vercelploy@gmail.com>
2025-10-22 13:15:12 +0300

    Admin infomly

--- components/nav-secondary.tsx
+++ components/nav-secondary.tsx
@@ -22,19 +22,21 @@ export function NavSecondary({
   }[]
 } & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
-    <SidebarGroup {...props}>
-      <SidebarGroupContent>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              {/* <SidebarMenuButton tooltip={item.title} href={item.url}>
-                <item.icon />
-                <span>{item.title}</span>
-              </SidebarMenuButton> */}
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroupContent>
-    </SidebarGroup>
+
+    <></>
+    // <SidebarGroup {...props}>
+    //   <SidebarGroupContent>
+    //     <SidebarMenu>
+    //       {items.map((item) => (
+    //         <SidebarMenuItem key={item.title}>
+    //           {/* <SidebarMenuButton tooltip={item.title} href={item.url}>
+    //             <item.icon />
+    //             <span>{item.title}</span>
+    //           </SidebarMenuButton> */}
+    //         </SidebarMenuItem>
+    //       ))}
+    //     </SidebarMenu>
+    //   </SidebarGroupContent>
+    // </SidebarGroup>
   )
 }
